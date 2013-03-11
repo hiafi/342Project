1265ac24647999ec9d1926584f274bc790302b52
Jonathan <hiafi3@gmail.com>
2013-03-11 13:10:26 -0700

    Removed draw

--- draw.js
+++
@@ -1,7 +0,0 @@
-function draw_number(context, number, x1, y1, x2, y2)
-{
-    context.fillStyle = '#000';
-    context.strokeStyle = '#000';
-    context.lineWidth = 2;
-    context.strokeRect(x1, y1, x2, y2);
-}
